800ba009246f683fb57f045f9811ed4c11b218aa
Kaer88 <robert.koncz88@gmail.com>
2023-07-06 15:45:48 +0200

    add react-bootstrap

--- frontend/todo-frontend/src/main.jsx
+++ frontend/todo-frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import LandingPage from './pages/LandingPage.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { CookiesProvider } from 'react-cookie'
-import { AuthProvider } from './contexts/authContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import LandingPage from './pages/LandingPage.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import { AuthProvider } from './contexts/authContext.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const router = createBrowserRouter([
   {
@@ -16,7 +17,7 @@ const router = createBrowserRouter([
     path: "/protected",
     element: <App />
   }
-])
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
@@ -26,4 +27,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </AuthProvider>
 
 
-)
+);
