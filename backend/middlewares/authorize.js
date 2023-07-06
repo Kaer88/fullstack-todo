8a31181200f86e1324aff4deb021907805235712
Kaer88 <robert.koncz88@gmail.com>
2023-07-06 20:35:51 +0200

    more cleanup

--- backend/middlewares/authorize.js
+++ backend/middlewares/authorize.js
@@ -3,7 +3,6 @@
 export default async (req, res, next) => {
     const userId = req.params.id;
     const tokenUserId = req.user.id;
-    console.log(userId, tokenUserId)
     try {
         if (userId != tokenUserId) throw new Error("invalid user token pair")
         next()
